5e68a16304aa2801322951a6ca3e018dce8373ae
RushikeshT14 <rushitodkari0108@gmail.com>
2025-11-21 09:34:36 +0530

    completed template

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+let NavLinks= document.querySelector(".nav-links");
+let NavLogo = document.querySelector(".nav-logo");
+
+NavLogo.addEventListener("click", () => {
+  NavLinks.classList.toggle("show");
+});
+
+
+ var swiper = new Swiper(".mySwiper", {
+      direction: "horizontal",
+      loop: true,
+
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      slidesPerView: 2,
+      spaceBetween: 20,
+      breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    }
+  }
+    });

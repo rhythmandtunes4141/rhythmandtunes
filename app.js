ba22c12872128b304acfef70f659f55ef41d3c5c
RushikeshT14 <rushitodkari0108@gmail.com>
2026-03-16 23:46:37 +0530

    gallery update

--- app.js
+++ app.js
@@ -33,3 +33,24 @@ NavLogo.addEventListener("click", () => {
     }
   }
     });
+
+
+const images = document.querySelectorAll(".gallery-item img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.onclick = () => {
+  lightbox.style.display = "none";
+};
+
+lightbox.onclick = () => {
+  lightbox.style.display = "none";
+};
